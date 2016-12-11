1bdeff5b3af6fe88266c60a9045db5304471f04e
Mark OBrien <mark_noreply@noreply_jh.com>
2016-12-11 09:27:20 -0500

    stuff seems to be working. totally redid slack gateway

--- slack_gateway.py
+++ slack_gateway.py
@@ -23,23 +23,52 @@ users[SLACK_USER_AMEH] = {'id':1,'displayName': "Ameh", 'insituCustomerId':48929
 users[SLACK_USER_RAXESH] = {'id':2,'displayName': "Raxesh", 'insituCustomerId':489299}
 users[SLACK_USER_JULIAN] = {'id':3,'displayName': "Julian", 'insituCustomerId':489299}
 
-def handle_command(command, channel, slack_user_id):
-    """
-        Receives commands directed at the bot and determines if they
-        are valid commands. If so, then acts on the commands. If not,
-        returns back what it needs for clarification.
-    """
-    if "track open orders" == nlp_understand(command):
+pendingQuestion = ""
+
+def handle_question_response(pendingQuestion):
+    if pendingQuestion != "":
+        response = "The last question I asked you was " + pendingQuestion
+        slack_client.api_call("chat.postMessage", channel=channel,
+            text=response, as_user=True)
+    return ""
+
+def handle_declaration_response(command,slack_user_id):
+    retVal = "" #if no question is asked don't return a value to be buffered by the function call
+    if nlp_understand(command) == "greetings":
+        print "Entering greeting"
+        #dynamic data in API makes it impossible to construct stable use cases for demo (recently ordered vs not)
+        #demo use case will always be not recently placed any orders
+        response = "Hello, " + users[slack_user_id]['displayName'] + " I see you have not ordered from us recently would you like to review your profile information?\n\n1. Yes\n2. No"
+        retVal = response
+        slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
+    elif nlp_understand(command) == "track open orders":
         print "Entering track open orders"
         response = "Now performing " + nlp_understand(command) + " for " + users[slack_user_id]['displayName'] + " ..."
+        mySaidRecently = response
         slack_client.api_call("chat.postMessage", channel=channel,
             text=response, as_user=True)
         customer_id = users[slack_user_id]['insituCustomerId']
         shipment_locations = pe.get_shipment_locations(customer_id)
         pe.upload_open_orders(customer_id,shipment_locations)
     else:
-        response = "homey don't play that"
+        response = "I'm still learning and don't understand " + command
         slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
+    return retVal #if a question was asked pass to super for buffering
+
+def handle_command(command, channel, slack_user_id, pendingQuestion):
+    """
+        Receives commands directed at the bot and determines if they
+        are valid commands. If so, then acts on the commands. If not,
+        returns back what it needs for clarification.
+    """
+    print "handle_command: " + command
+    retVal = ""
+    if pendingQuestion != "":
+        retVal = handle_question_response(pendingQuestion)
+    else:
+        retVal = handle_declaration_response(command,slack_user_id)
+    print pendingQuestion
+    return retVal
 
 def parse_slack_output(slack_rtm_output):
     """
@@ -57,15 +86,17 @@ def parse_slack_output(slack_rtm_output):
                        output['user']
     return None, None, None
 
-def nlp_understand(please_do_this):
+def nlp_understand(given_phrase):
     retVal = ""
-    if (please_do_this == "track open orders"):
+    if (given_phrase == "track open orders"):
         retVal = "track open orders"
-    elif (please_do_this == "too"):
+    elif (given_phrase == "too"):
         retVal = "track open orders"
-    #elif whatever nlp distributed live search finds for synonyms error handling 
+    #elif all other phrases that could mean track open orders return 'track open orders'
+    set_of_greetings = set(['hi', 'hello', 'hey', 'heya', 'whatup', 'greetings', 'hola'])
+    if (given_phrase in set_of_greetings):
+        retVal = "greetings"
     return retVal
-
 
 if __name__ == "__main__":
     READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
@@ -74,7 +105,7 @@ if __name__ == "__main__":
         while True:
             command, channel, user = parse_slack_output(slack_client.rtm_read())
             if command and channel and user:
-                handle_command(command, channel, user)
+                pendingQuestion = handle_command(command, channel, user, pendingQuestion)
             # TODO add error handling
     else:
         print("Connection failed. Bad internet connection? Invalid Slack token or bot ID?")
