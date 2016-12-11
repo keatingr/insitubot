6dff0b10a86ef4bdf86fae18750b7b1198a109c2
patelraxesh <raxesh4you@gmail.com>
2016-12-10 20:35:32 -0500

    added utility functions and ipython notebook

--- run.py
+++ run.py
@@ -5,11 +5,19 @@ app = Flask(__name__)
 
 @app.route("/sms", methods=['POST'])
 def hello():
-	gettext = request.form["Body"]
-	num = request.form["From"]
+	in_text = request.form["Body"]
+	number = request.form["From"]
+	return_text = get_response(in_text)
 	response = twiml.Response()
-	response.message(gettext + " from "+ num)
+	response.message(return_text)
 	return str(response)
+
+def get_response(input_text):
+	"""
+	Takes in input text and get reply from bot
+	"""
+	return input_text
 
 if __name__ == "__main__":
     app.run(debug=True)
+
